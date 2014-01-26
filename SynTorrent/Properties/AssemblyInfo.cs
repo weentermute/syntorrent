15b6985535873e1352d5f8378d0e19cc3ac74e78
w1ntermute <w1ntermute@w1ntermute.fake>
2014-01-26 17:14:42 +0100

    - configured ClickOnce publishing

--- SynTorrent/Properties/AssemblyInfo.cs
+++ SynTorrent/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SynTorrent")]
-[assembly: AssemblyCopyright("Copyright ©  2013 Emilio Cherubini")]
+[assembly: AssemblyCopyright("Copyright © 2014 Emilio Cherubini")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
